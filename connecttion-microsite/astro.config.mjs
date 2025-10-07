167f4fb572c5a4d1f856a50013bdb295923dc331
eduardojfrosario <eduardojfrosario@gmail.com>
2025-10-07 18:25:18 +0100

    change template

--- connecttion-microsite/astro.config.mjs
+++ connecttion-microsite/astro.config.mjs
@@ -1,18 +1,9 @@
-import tailwindcss from "@tailwindcss/vite";
-// @ts-check
-
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import react from "@astrojs/react";
+import bookshop from '@bookshop/astro-bookshop';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  site: "https://top-quail.cloudvent.net/",
+  integrations: [bookshop(), react()]
 });
--- connecttion-microsite/src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Site Author – Personal Blog & Portfolio';
-export const SITE_DESCRIPTION = 'Growth engineering, analytics, and product marketing notes. Case studies, experiments, and projects by Site Author.';
